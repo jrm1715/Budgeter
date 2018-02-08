6061747f3027850f76419466099afe07dfe796d8
Jason Mattis <rocknrollx18@aim.com>
2018-02-07 19:35:38 -0800

    Added functionality to the 'type' button for expense type, expense description, and expense value.

--- app.js
+++ app.js
@@ -6,26 +6,48 @@ var budgetController = (function() {
 
 // UI Controller
 var UIController = (function() {
-  // Some code
+
+  var DOMStrings = {
+    inputType: '.add__type',
+    inputDesc: '.add__description',
+    inputValue: '.add__value',
+    inputBtn: '.add__btn'
+  };
+
+  return {
+    getInput: function() {
+      return {
+         type: document.querySelector(DOMStrings.inputType).value, // Will be either inc or exp
+         description: document.querySelector(DOMStrings.inputDesc).value,
+         value: document.querySelector(DOMStrings.inputValue).value,
+      };
+    },
+    getDOMStrings: function() {
+      return DOMStrings;
+    }
+  };
 })();
 
 // Global App Controller
 var controller = (function(budgetCtrl, UICtrl) {
+  var DOM = UICtrl.getDOMStrings();
 
   var ctrlAddItem = function() {
     // TODO 1. Get the field input data
+    var input = UICtrl.getInput();
+    console.log(input);
+
     //      2. Add the item to the budget Controller
     //      3. Add the item to the UI
     //      4. Calculate budget
     //      5. Display the budget on the UI
   }
-
-  document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+  document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
   document.addEventListener('keypress', function(event) {
     if (event.keyCode === 13 || event.which === 13) {
       ctrlAddItem();
     }
-  });
 
+  });
 })(budgetController, UIController);
